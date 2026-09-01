db0991452e52182fa6daac41455eed2a9a8f90f2
Mark Melotto <70904313+MarkMelotto@users.noreply.github.com>
2026-09-01 12:49:29 +0200

    Update bmi_image container reference to GitHub registry

--- src/ewatercycle_pcrglobwb/model.py
+++ src/ewatercycle_pcrglobwb/model.py
@@ -50,7 +50,7 @@ class PCRGlobWB(ContainerizedModel):
 
     forcing: Optional[PCRGlobWBForcing] = None
     parameter_set: ParameterSet  # not optional for this model
-    bmi_image: ContainerImage = ContainerImage("ewatercycle/pcrglobwb-grpc4bmi:v0.2.0")
+    bmi_image: ContainerImage = ContainerImage("ghcr.io/ewatercycle/pcrglobwb-grpc4bmi:v0.2.0")
 
     _config: CaseConfigParser = PrivateAttr()
 
